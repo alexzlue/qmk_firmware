0c6de2d345c1d39a9b6a4a8b8b89b63fdba31999
Alex Lue <a.lue820@gmail.com>
2021-07-07 22:14:31 -0700

    via

--- keyboards/gmmk/pro/keymaps/alue/keymap.c
+++ keyboards/gmmk/pro/keymaps/alue/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
+/* Copyright 2021 alexzlue
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
